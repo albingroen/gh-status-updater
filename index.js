00cf0ef02f058930a76d8a45ed420b6e91f9c539
Albin Groen <albin.groen@gmail.com>
2021-05-24 21:43:45 +0200

    :bug: Fix check

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const main = () => {
     throw Error("Please enter a message and emoji");
   }
 
-  if (!process.env.GITHUB_ACCESS_TOKEN || !token) {
+  if (!process.env.GITHUB_ACCESS_TOKEN && !token) {
     throw Error("Missing GitHub access token");
   }
 
